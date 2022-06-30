e25d8f10fb7f55ee4ba6ef4e3c88bcdf548541e1
tuanvh262 <tuanvh@vnext.com.vn>
2022-07-01 01:55:22 +0700

    change color code response

--- app/serializers/label_serializer.rb
+++ app/serializers/label_serializer.rb
@@ -2,6 +2,6 @@ class LabelSerializer < ActiveModel::Serializer
   attributes :id, :category_name, :color_code, :category_code, 
 
   def color_code
-    "rgb(#{object.color_code.gsub(/[^\d]/, '').split('').map { |e| (e.to_f * 255).to_i }.join(',')})"
+    "rgb(#{object.color_code.gsub(/[^\d]/, '').split('').map { |e| (((e.to_i == 0) ? 0 : (1/e.to_f)) * 255).to_i }.join(',')})"
   end
 end
